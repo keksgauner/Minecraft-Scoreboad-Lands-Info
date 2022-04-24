393d99f7ae290e8def0555854b1238733c6d242c
KeksGauner <darklupo5.2003@gmail.com>
2022-04-24 04:06:51 +0200

    Fix beim Verlassen keine Aktualisierung zum Wilderness

--- src/main/java/de/keksgauner/UpdateScoreboard.java
+++ src/main/java/de/keksgauner/UpdateScoreboard.java
@@ -6,28 +6,47 @@ import me.angeschossen.lands.api.land.Area;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.ArrayList;
 
 public class UpdateScoreboard implements org.bukkit.event.Listener {
+
+    // Es soll nicht doppelt aufgerufen werden
+    private static ArrayList<Player> players;
 
     @EventHandler
     public void onPlayerLocationAreaEvent(PlayerLocationAreaEvent event) {
         Player player = event.getLandPlayer().getPlayer();
         Area area = event.getArea();
 
-        String owner = Bukkit.getOfflinePlayer(event.getArea().getOwnerUID()).getName();
-        assert owner != null;
-        if (owner.equalsIgnoreCase(player.getName()))
-            new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_owner", "&3Dein Land");
-        else
-            new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_owner", "&6" + owner);
+        if(!players.contains(player)) {
+            String owner = Bukkit.getOfflinePlayer(event.getArea().getOwnerUID()).getName();
+            assert owner != null;
+            if (owner.equalsIgnoreCase(player.getName()))
+                new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_owner", "&3Dein Land");
+            else
+                new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_owner", "&6" + owner);
 
-        new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_name", "&9" + area.getLand().getName());
+            new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_name", "&9" + area.getLand().getName());
+            players.add(player);
+        }
     }
 
     @EventHandler
     public void onPlayerAreaLeaveEvent(PlayerAreaLeaveEvent event) {
         Player player = event.getLandPlayer().getPlayer();
+        if(players.contains(player))
+            players.remove(player);
         new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_owner", "&aWilderness");
         new CreateScoreboad(player.getScoreboard()).setTeamPrefix("lands_name", "");
+    }
+
+    // Wenn ein Spieler den Server verlässt, wird der Spieler falls nötig aus der Liste entfernt
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if(players.contains(player))
+            players.remove(player);
     }
 }
